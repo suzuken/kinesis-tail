8aa8451ad9ce2c09ad917599abdd7736cc5e244b
Kenta Suzuki <suzuken326@gmail.com>
2015-12-01 14:12:56 +0900

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# kinesis-tail
+
+Continuously fetch records from Kinesis Stream and print it. Useful for debugging Kinesis.
+
+## How to use
+
+	$ go get github.com/suzuken/kinesis-tail
+	$ kinesis-tail -stream your-kinesis-stream-name -region ap-northeast-1 -max-item-size=512
+	Your Stream information: {
+	  StreamDescription: {
+		HasMoreShards: false,
+		RetentionPeriodHours: 24,
+		Shards: [{
+			HashKeyRange: {
+			  EndingHashKey: "340282366920938463463374607431768211455",
+			  StartingHashKey: "0"
+			},
+			SequenceNumberRange: {
+			  StartingSequenceNumber: "49556705040321186153938184833717233207750712554199973890"
+			},
+			ShardId: "shardId-000000000000"
+		  }],
+		StreamARN: "arn:aws:kinesis:ap-northeast-1:xxxxxxxxxxxx:stream/your-kiness-stream-name",
+		StreamName: "your-kiness-stream-name",
+		StreamStatus: "ACTIVE"
+	  }
+	}
+	{
+	  ShardIterator: "AAAAAAAAAAF6jxGy30eyuTD4GzFrqAwEUFdWMIiWtFqpzGIShgXfo40VHDGHzNAdN0uvK5j6llWkEJQtdKZC3kQTIOQ/Y7Zj/lyBrV4GTkqz/hT+m/8TCZHU61fbEqU7Wd9V+Wa9szmeBQfdN5BAZDybhSUctjJRT+mgfbxVPXOjLkIrBNpAvzPFLRRaNSJj/ZwxIcG3k3nfwY7yUrcNb42OR6Cu7o1P"
+	}
+	ApproximateArrivalTimestamp: 2015-12-01 02:14:23 +0000 UTC
+	Data: {"id":"5310e2ef-25e3-4942-87df-2aed28320b99","hoge":"fuga"}
+	SequenceNumber: 49556846757453585699518614739447433851727187225060835330
+	ApproximateArrivalTimestamp: 2015-12-01 02:24:41 +0000 UTC
+	Data: {"id":"75ba2e4a-a68d-4d50-89cf-8b954a115a08","hoge":"fuga"}
+	SequenceNumber: 49556846757453585699518614739460732035742990683338702850
+	ApproximateArrivalTimestamp: 2015-12-01 04:10:58 +0000 UTC
+	Data: {"id":"0d669fb6-045e-4299-946c-5786037ab638","hoge":"fuga"}
+	...
+
+## How does it works:
+
+* At first, describe stream.
+* Get shart iterator.
+* `GetRecords` iteratively.
+
+## LICENSE
+
+MIT
+
+## Author
+
+Kenta Suzuki (a.k.a suzuken)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"os"
+	"time"
+)
+
+var (
+	stream       = flag.String("stream", "your-stream", "your stream name")
+	region       = flag.String("region", "ap-northeast-1", "the AWS region where your Kinesis Stream is.")
+	iteratorType = flag.String("iterator-type", "TRIM_HORIZON", "iterator type. Choose from TRIM_HORIZON(default), AT_SEQUENCE_NUMBER, or LATEST.")
+	maxItemSize  = flag.Int("max-item-size", 4096, "max byte size per item for printing.")
+)
+
+func main() {
+	flag.Parse()
+
+	s := session.New(&aws.Config{Region: aws.String(*region)})
+	kc := kinesis.New(s)
+
+	streamName := aws.String(*stream)
+
+	streams, err := kc.DescribeStream(&kinesis.DescribeStreamInput{StreamName: streamName})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot describe stream. please verify your stream is accecible.: %s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Your Stream information: %v\n", streams)
+
+	iteratorOutput, err := kc.GetShardIterator(&kinesis.GetShardIteratorInput{
+		// take first shard.
+		ShardId:           streams.StreamDescription.Shards[0].ShardId,
+		ShardIteratorType: aws.String(*iteratorType),
+		StreamName:        streamName,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot get iterator: %s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%v\n", iteratorOutput)
+
+	if err := iter(kc, iteratorOutput.ShardIterator, *maxItemSize); err != nil {
+		fmt.Fprintf(os.Stderr, "get records failed: %s", err)
+		os.Exit(1)
+	}
+}
+
+// iter fetch records.
+func iter(kc *kinesis.Kinesis, shardIterator *string, maxItemSize int) error {
+	records, err := kc.GetRecords(&kinesis.GetRecordsInput{
+		ShardIterator: shardIterator,
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "get records failed: %s", err)
+		os.Exit(1)
+	}
+	for _, r := range records.Records {
+		fmt.Printf("ApproximateArrivalTimestamp: %v\n", r.ApproximateArrivalTimestamp)
+		fmt.Printf("Data: %s\n", r.Data[:maxItemSize])
+		fmt.Printf("SequenceNumber: %s\n", *r.SequenceNumber)
+	}
+	time.Sleep(time.Second * 3)
+	return iter(kc, records.NextShardIterator, maxItemSize)
+}
